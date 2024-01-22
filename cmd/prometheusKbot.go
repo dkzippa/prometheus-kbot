2da5c770ddabd277c98d2dd77f1b06194641e783
Andrii Yermienieu <dkzippa@gmail.com>
2024-01-22 17:35:22 +0100

    fixes to the app

--- cmd/prometheusKbot.go
+++ cmd/prometheusKbot.go
@@ -5,10 +5,11 @@ package cmd
 
 import (
 	"fmt"
-	"gopkg.in/telebot.v3"
 	"log"
 	"os"
 	"time"
+
+	"gopkg.in/telebot.v3"
 
 	"github.com/spf13/cobra"
 )
@@ -19,15 +20,10 @@ var (
 
 // prometheusKbotCmd represents the prometheusKbot command
 var prometheusKbotCmd = &cobra.Command{
-	Use:     "prometheusKbot",
+	Use:     "kbot",
 	Aliases: []string{"start"},
-	Short:   "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short:   "starts the application",
+	Long:    ``,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		fmt.Printf("kbot %s started", appVersion)
